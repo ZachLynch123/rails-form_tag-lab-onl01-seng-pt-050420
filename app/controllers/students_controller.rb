fef28071e7642df6a38d0e31da58c1a22e3a8b77
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-09-12 20:22:08 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   end
   
   def create 
+    binding.pry
     Student.create(first_name: params[:student][:first_name], last_name: [:student][:last_name])
         Post.create(title: params[:post][:title], description: params[:post][:description])
 
