8dcf72cd06c9fb4fc536478d540a9390cbb5938c
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-09-12 19:52:29 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,15 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new 
+  end
+  
+  def create 
+    
+    
+    
+    redirect_to students_path
+  end
 
 end
