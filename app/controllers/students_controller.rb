93b025bff66a2235dd1c9e9942846ff29635fb56
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-09-12 20:22:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,8 +14,6 @@ class StudentsController < ApplicationController
   def create 
     binding.pry
     Student.create(first_name: params[:student][:first_name], last_name: [:student][:last_name])
-        Post.create(title: params[:post][:title], description: params[:post][:description])
-
     
     
     redirect_to students_path
